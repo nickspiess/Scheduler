e01244efee6e47cd4d422a6fea729e8fdd85a094
nickspiess <nickspiess23@gmail.com>
2021-11-16 20:15:04 -0600

    Add files via upload

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
@@ -23,17 +24,68 @@ public:
     int processPri;
     int processDline;
     int processIO;
-    cout << "Process PID" << processPID << endl;
 };
+
+
+
+// This function is to create and return a vector of processes
+vector<Process> processCreator(string fileName) {
+    // Create a vector of all our processes
+    vector<Process> processes;
+
+    // Open input file stream
+    std::ifstream in(fileName);
+    // Declare line
+    string line;
+    // While the file still has lines:
+    while (std::getline(in, line)) {
+
+        // Grabbing the input from the line
+        in >> pid >> bst >> arr >> pri >> dline >> IO;
+
+        // Sanitizing input
+        if (pid >= 0 && bst >= 0 && arr >= 0 && pri >= 0 && dline >= 0 && IO >= 0) {
+            // Creating a process
+            Process process;
+            process.processPid = pid;
+            process.processBst = bst;
+            process.processArr = arr;
+            process.processPri = pri;
+            process.processDline = dline;
+            process.processIO = IO;
+            // Add process to our vector queue
+            processes.push_back(process);
+        }
+
+        // We will sort our processes
+        //cout << "PID = " << one.processPid << " BST = " << one.processBst << " arr = " << one.processArr << " pri = " << one.processPri << " dline = " << one.processDline << " IO = " << one.processIO << endl;
+                
+                //queues[0].push(one);
+                //cout << queues[0].front();
+                
+
+                // Call mfqs corresponding functions according to these variables
+                //
+            }
+
+    return processes;
+}
+
+
+bool compareArrival(const Process beg, const Process end){
+    return beg.processArr > end.processArr;
+}
+
 
 int main()
 {
     
-    Process one;
     
     string programType;
     int numQueues;
     string fileName;
+    int ageInterval;
+    int timeQuantum;
     
     std::cout << "Welcome to our Scheduling Algorithm Program\n";
     std::cout << "Please select which algorithm you'd like to run.\n Type 'a' for a Multi-Level Feedback Queue Scheduler (MFQS)\n Type 'b' for a Real-Time Scheduler (RTS):" << endl;
@@ -42,8 +94,9 @@ int main()
     // Check number of queues
     cout << "You entered: " << programType << endl;
         
-        
-    
+    // Vector queue of our processes using the Process class
+    vector<Process> processesCreated;
+
     // MFQS
     if (programType == "a") {
         // declare input string
@@ -57,10 +110,15 @@ int main()
         cout << "You entered: " << numQueues << endl;
 
         // Create and initialize vector container of queues based on user input
-        std::vector<std::queue<int> > queues(numQueues);
-        //vector<queue<int>> vec;
+        vector<std::queue<int> > queues(numQueues);
+        
+        // Create a vector of processes
+        vector<Process> processes;
+        
+
         // Can be accessed through queues[0], queues[1], ...,  queues[4]
         
+
         // Check number of queues
         // Correct number of queues
         if (numQueues >= 1 || numQueues <= 5) {
@@ -68,30 +126,45 @@ int main()
             // Grab file name
             std::cin >> fileName;
 
-            // Open input file stream
-            std::ifstream in(fileName);
-            // Declare line
-            string line;
-            // While the file still has lines:
-            while (std::getline(in, line)) {
+            // Add all processes to queue of processes from the returned queue of processes
+            processes = processCreator(fileName);
 
-                in >> pid >> bst >> arr >> pri >> dline >> IO;
+            cout << "processes size: = " << processes.size();
+            //cout << "created processes size: = " << processesCreated.size();
 
-                one.processPid = pid;
-                one.processBst = bst;
-                one.processArr = arr;
-                one.processPri = pri;
-                one.processDline = dline;
-                one.processIO = IO;
-                //cout << "PID = " << one.processPid << " BST = " << one.processBst << " arr = " << one.processArr << " pri = " << one.processPri << " dline = " << one.processDline << " IO = " << one.processIO << endl;
-                
-                queues[0].push(one);
-                cout << queues[0].front();
-                
-
-                // Call mfqs corresponding functions according to these variables
-                //
+            if(processes.size() > 0) {
+                //for(int i = 0; i < createdProcesses.size(); i++) {
+                    //Process OldProc = *
+                //}
             }
+
+            // We now will sort our processes baseed on arrival time.
+            sort(processes.begin(), processes.end(), compareArrival);
+
+            cout << "Grabbing MFQS Information:\n";
+            cout << "What is the age interval?:\n";
+            cin >> ageInterval;
+            cout << "What is the Time Quantum?:\n";
+            cin >> timeQuantum;
+            
+            // size of processes array
+            int size = processes.size();
+            std::cout << "size = " << size << "\n";
+            Process proc;
+            //proc = processes.begin();
+            //cout << "PID = " << proc.processPid;
+
+
+            //for (int i = 0; i <= size; i++) {
+             //   cout << "PID = " << processes.begin().processPid;
+                //<< " BST = " << bst << " arr = " << arr << " pri = " << pri << " dline = " << dline << " IO = " << IO << endl;
+            //}
+
+            //cout << "PID = " << pid << " BST = " << bst << " arr = " << arr << " pri = " << pri << " dline = " << dline << " IO = " << IO << endl;
+
+
+
+
         }
         // incorrect # of queues, exit
         else {
